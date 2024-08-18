90f2d82cacfa79d16d4be137ff85d8cd1a49a27b
qinran0423 <1156107187@qq.com>
2024-08-18 22:46:38 +0800

    组件状态

---
+++ cli-create/src/Tick.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+
+export default class Tick extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      left: this.props.num
+    }
+    this.timer = setInterval(() => {
+      this.setState({
+        left: this.state.left - 1
+      })
+			if(this.state.left === 0) {
+				clearInterval(this.timer)
+			}
+    }, 1000)
+  }
+  render() {
+    return <h1>倒计时剩余时间：{this.state.left}</h1>
+  }
+}
--- cli-create/src/index.jsx
+++ cli-create/src/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MyFuncComp from './MyFunComp'
 import MyClassComp from './MyClassComp'
+import Tick from './Tick'
+import MyStateComp from './MyStateComp'
 
 ReactDOM.render(<div>
-	<MyFuncComp num={2}/>
+	{/* <MyFuncComp num={2}/>
 	<MyClassComp num={3} enable obj={{
 		name: 'mick',
 		age: 18
 	}}
 	ui={<h2>传递一个UI</h2>}
-	/>
+	/> */}
+	<Tick num={10}/>
+	<MyStateComp />
 </div>, document.getElementById('root'));
