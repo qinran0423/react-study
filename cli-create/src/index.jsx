42376dfcdf86c9f8bc225827000906765291e8de
qinran0423 <1156107187@qq.com>
2024-08-20 23:30:31 +0800

    ball demo

--- cli-create/src/index.jsx
+++ cli-create/src/index.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyFuncComp from './MyFunComp'
-import MyClassComp from './MyClassComp'
-import Tick from './Tick'
-import MyStateComp from './MyStateComp'
+import BallList from './components/ballList'
+
+
 
 ReactDOM.render(<div>
-	{/* <MyFuncComp num={2}/>
-	<MyClassComp num={3} enable obj={{
-		name: 'mick',
-		age: 18
-	}}
-	ui={<h2>传递一个UI</h2>}
-	/> */}
-	<Tick num={10}/>
-	<MyStateComp />
+	<BallList/>
 </div>, document.getElementById('root'));
